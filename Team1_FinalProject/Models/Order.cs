e0a42fdc0b61349d95cb522f3fd01c4e967721c7
nadia.baig <70566188+nadbagel@users.noreply.github.com>
2020-11-05 19:01:04 -0600

    order and orderdetail models

--- Team1_FinalProject/Models/Order.cs
+++ Team1_FinalProject/Models/Order.cs
@@ -1,10 +1,32 @@
-﻿using System;
+using System;
 namespace Team1_FinalProject.Models
 {
     public class Order
     {
         public Order()
         {
+	
+	[Display(Name = "OrderID")]
+	public Int32 OrderID {get; set;}
+
+	[Display(Name = "Is this a gift order?]	
+	public Boolean GiftOrder {get; set;}
+	
+	[Display(Name = Do you want to use Popcorn Points?"]
+	public Boolean PopcornPoints {get; set;}
+	
+	public DateTime {get; set;}
+
+	public List<OrderDetails> OrderDetails {get; set;}
+
+	public Order()
+        {
+            if (OrderDetails == null)
+            {
+                OrderDetails = new List<OrderDetails>();
+            }
+        }
+
         }
     }
 }
--- Team1_FinalProject/Models/OrderDetail.cs
+++ Team1_FinalProject/Models/OrderDetail.cs
@@ -1,10 +1,20 @@
-﻿using System;
+using System;
 namespace Team1_FinalProject.Models
 {
     public class OrderDetail
     {
         public OrderDetail()
         {
+	
+	public Int32 OrderDetailID {get; set;}
+	
+	[Display(Name = "Seat Number:")]
+	public String SeatNumber {get; set;}
+
+	public Order Order {get; set;}
+
+	public Showing Showing {get; set;}
+
         }
     }
 }
