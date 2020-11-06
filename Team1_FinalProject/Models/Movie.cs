c312f3155283a85b7204c430c1531bd2e6b64d48
Alice Yang <alicejjyang@gmail.com>
2020-11-05 18:57:02 -0600

    Created Movie/MovieReview.cs

--- Team1_FinalProject/Models/Movie.cs
+++ Team1_FinalProject/Models/Movie.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace Team1_FinalProject.Models
 {
     public class Movie
     {
         public Movie()
         {
+			public Int32 MovieID { get; set; }
+			public string MovieName { get; set; }
+			public DateTime MovieLength { get; set; }
+			public string MovieDescription { get; set; }
+			public Genre Genre { get; set; }
+			public List<Showing> Showings { get; set; }
         }
     }
 }
--- Team1_FinalProject/Models/MovieReview.cs
+++ Team1_FinalProject/Models/MovieReview.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace Team1_FinalProject.Models
 {
     public class MovieReview
     {
         public MovieReview()
         {
+			public MovieReviewID { get; set; }
+			[Range(1, 5, ErrorMessage = "Rating must be between 1-5.")]
+			public MovieRating { get; set; }
+			[StringLength(280, ErrorMessage = "Character limit is 280 characters.")]
+			public ReviewDescription { get; set; }
+			public AppUser User { get; set; }
+			public Movie Movie { get; set; }
         }
     }
 }
