f1fad16430dafc07deeb1d990fa64f7541512bd7
EricLin38 <45245999+EricLin38@users.noreply.github.com>
2020-11-05 18:31:42 -0600

    added model

---
+++ Team1_FinalProject/Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lin_Eric_HW4.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser() 
+        {
+        }
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+    }
+}
