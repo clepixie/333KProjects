193eb0e1809c875dd2645f09c90c5e9b63a8cccd
EricLin38 <45245999+EricLin38@users.noreply.github.com>
2020-11-05 18:21:56 -0600

    added shit

--- Team1_FinalProject/Startup.cs
+++ Team1_FinalProject/Startup.cs
@@ -1,40 +1,65 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace Team1_FinalProject
+//TODO: Make this namespace match your project - be sure to remove the []
+namespace [Your_Project_Name]
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //This adds the MVC engine and Razor code
+            services.AddControllersWithViews();
+
+             //TODO: (For HW4 and beyond) Add a connection string here once you have created it on Azure
+            //String connectionString = "";
+
+            //TODO: Uncomment this line once you have your connection string
+            //services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+            //TODO: Uncomment these lines once you have added Identity to your project
+            ////NOTE: This is where you would change your password requirements
+            /*services.AddIdentity<AppUser, IdentityRole>(opts => {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();*/
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //These lines allow you to see more detailed error messages
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
 
+            //TODO: Once you have added Identity into your project, you will need to uncomment this line
+            //app.UseAuthentication();
+
+            //This line allows you to use static pages like style sheets and images
+            app.UseStaticFiles();
+
+            //This marks the position in the middleware pipeline where a routing decision
+            //is made for a URL.
             app.UseRouting();
 
+            //This method maps the controllers and their actions to a patter for
+            //requests that's known as the default route. This route identifies
+            //the Home controller as the default controller and the Index() action
+            //method as the default action. The default route also identifies a 
+            //third segment of the URL that's a parameter named id.
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
+   
+
 }
