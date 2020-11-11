ef309f99b58f99dc2c0c5636a80c4ae6aae52b7e
nadia.baig <70566188+nadbagel@users.noreply.github.com>
2020-11-11 12:59:23 -0600

    updated

--- Team1_FinalProject/Models/ViewModels/SearchViewModel.cs
+++ Team1_FinalProject/Models/ViewModels/SearchViewModel.cs
@@ -1,22 +1,18 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-<<<<<<< HEAD
-namespace Team1_FinalProject.Models
-=======
 
-namespace Team1_FinalProject.Models.ViewModels
->>>>>>> fe5f9a8070edbe4e958a8e79867c5b840db46061
+namespace Team1_FinalProject.Models
+
 {
-    public enum Ratings
+	public enum Ratings
 	{
 		Greater,
 		Lesser
 	}
 
 	public class SearchViewModel
-    {
-<<<<<<< HEAD
-        [Display(Name = "Title:")]
+	{
+		[Display(Name = "Title:")]
 		public string SearchTitle { get; set; }
 		[Display(Name = "Tagline:")]
 		public string SearchTagline { get; set; }
@@ -29,37 +25,10 @@ namespace Team1_FinalProject.Models.ViewModels
 		[Display(Name = "Actor:")]
 		public String SearchActor { get; set; }
 		[Display(Name = "Customer Rating:")]
-		[Range(1,5, ErrorMessage = "Rating must be between 1-5.")]
+		[Range(1, 5, ErrorMessage = "Rating must be between 1-5.")]
 		public Decimal? SearchRating { get; set; }
 		[Display(Name = "")]
 		public Ratings? RatingRange { get; set; }
-=======
-		public enum Ratings
-		{
-			Greater,
-			Lesser
-		}
-		
-        public SearchViewModel()
-        {
-			[Display(Name = "Title:")]
-			public string SearchTitle { get; set; }
-			[Display(Name = "Tagline:")]
-			public string SearchTagline { get; set; }
-			[Display(Name = "Genre:")]
-			public Int32 SelectGenreID { get; set; }
-			[Display(Name = "Date:")]
-			public DateTime? SearchDate { get; set; }
-			[Display(Name = "MPAA Rating:")]
-			public string SearchMPAA { get; set; }
-			[Display(Name = "Actor:")]
-			public SearchActor SearchActor { get; set; }
-			[Display(Name = "Customer Rating:")]
-			[Range(1,5, ErrorMessage = "Rating must be between 1-5.")]
-			public Decimal? SearchRating { get; set; }
-			[Display(Name = "")]
-			public Ratings? RatingRange { get; set; }
-        }
->>>>>>> fe5f9a8070edbe4e958a8e79867c5b840db46061
-    }
+
+	}
 }
