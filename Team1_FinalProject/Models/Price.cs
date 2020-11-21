ac2f45d09b12b2d76720a7883d7f282fd630bca8
Alice Yang <alicejjyang@gmail.com>
2020-11-21 13:49:09 -0600

    Helped with Checkout

--- Team1_FinalProject/Models/Price.cs
+++ Team1_FinalProject/Models/Price.cs
@@ -5,37 +5,22 @@ using System.Linq;
 using System.Collections.Generic;
 namespace Team1_FinalProject.Models
 {
+    public enum Type
+    {
+        Showing,
+        Discount
+    }
     public class Price
     {
         public Int32 PriceID { get; set; }
 
-        [Display(Name ="Matinee Price")]
-        [DisplayFormat(DataFormatString ="{0:c}")]
-        public Int32 MatineePrice
-        { get { return OrderSubtotal + Tax; }; set; }
+        public string PriceTitle { get; set; }
 
-        [Display(Name = "Tuesday Discount Price")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public Int32 DiscountPrice
-        { get; set; }
+        public Type PriceType { get; set; }
 
-        [Display(Name = "Regular Ticket Price")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public Int32 BasePrice
-        { get; set; }
-
-        [Display(Name = "Weekend Discount Price")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public Int32 WeekendPrice
-        { get; set; }
-
-        [Display(Name = "Senior Citizen Discount")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public Int16 SeniorDiscount
-        { get; set; }
+        public decimal PriceValue { get; set; }
 
         public List<Showing> Showings { get; set; }
 
-        
     }
 }
