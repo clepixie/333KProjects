efb237291ff1f3c2c1bfac82a916bc70c6d7743d
nadia.baig <70566188+nadbagel@users.noreply.github.com>
2020-11-22 13:47:25 -0600

    migration

---
+++ Team1_FinalProject/Migrations/20201122194701_Setup9.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Team1_FinalProject.Migrations
+{
+    public partial class Setup9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumber",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Orders");
+        }
+    }
+}
