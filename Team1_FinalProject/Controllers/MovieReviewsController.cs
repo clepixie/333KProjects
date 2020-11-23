78d52928ae181ed30232b72dbaf3b4ac335b96fb
Alice Yang <alicejjyang@gmail.com>
2020-11-23 12:28:14 -0600

    poked around in MovieReview

--- Team1_FinalProject/Controllers/MovieReviewsController.cs
+++ Team1_FinalProject/Controllers/MovieReviewsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Team1_FinalProject.DAL;
 using Team1_FinalProject.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Team1_FinalProject.Controllers
 {
@@ -20,9 +21,25 @@ namespace Team1_FinalProject.Controllers
         }
 
         // GET: MovieReviews
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.MovieReviews.ToListAsync());
+            List<MovieReview> movieReviews;
+            if (User.IsInRole("Employee") || User.IsInRole("Manager"))
+            {
+                movieReviews = _context.MovieReviews
+                                .Include(o => o.Movie)
+                                .ToList();
+            }
+            else //user is a customer, so only display their records
+            {
+                movieReviews = _context.MovieReviews
+                                .Include(o => o.Movie)
+                                .Where(o => o.User.UserName == User.Identity.Name)
+                                .ToList();
+            }
+
+            //
+            return View(movieReviews);
         }
 
         // GET: MovieReviews/Details/5
@@ -33,8 +50,8 @@ namespace Team1_FinalProject.Controllers
                 return NotFound();
             }
 
-            var movieReview = await _context.MovieReviews
-                .FirstOrDefaultAsync(m => m.MovieReviewID == id);
+            MovieReview movieReview = await _context.MovieReviews.Include(mr => mr.Movie)
+                                            .FirstOrDefaultAsync(m => m.MovieReviewID == id);
             if (movieReview == null)
             {
                 return NotFound();
@@ -44,9 +61,30 @@ namespace Team1_FinalProject.Controllers
         }
 
         // GET: MovieReviews/Create
-        public IActionResult Create()
+        public IActionResult Create(int? id)
         {
-            return View();
+            List<MovieReview> userreviews;
+            userreviews = _context.MovieReviews.Where(m => m.User.UserName == User.Identity.Name).ToList();
+            if (userreviews.Count() != 0)
+            {
+                foreach (MovieReview review in userreviews)
+                {
+                    if (review.Movie.MovieID == id)
+                    {
+                        return RedirectToAction("Edit", "MovieReviews", new { id = id });
+                    }
+                }
+            }
+            //create a new instance of the MovieReview class
+            MovieReview mr = new MovieReview();
+
+            //find the movie that should be associated with this MR
+            Movie dbMovie = _context.Movies.Find(id);
+
+            //set the new MR's movie equal to the movie you just found
+            mr.Movie = dbMovie;
+
+            return View(mr);
         }
 
         // POST: MovieReviews/Create
@@ -54,15 +92,21 @@ namespace Team1_FinalProject.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MovieReviewID,MovieRating,ReviewDescription")] MovieReview movieReview)
+        public async Task<IActionResult> Create([Bind("MovieReviewID,MovieRating,ReviewDescription,Movie")] MovieReview movieReview)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid == false)
             {
-                _context.Add(movieReview);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return View(movieReview);
             }
-            return View(movieReview);
+
+            Movie dbMovie = _context.Movies.Find(movieReview.Movie.MovieID);
+
+            movieReview.Movie = dbMovie;
+
+            _context.Add(movieReview);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Details", "MovieReviews", new { id = movieReview.MovieReviewID });
         }
 
         // GET: MovieReviews/Edit/5
@@ -70,13 +114,16 @@ namespace Team1_FinalProject.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return View("Error", new String[] { "Please specify a movie review to edit!" });
             }
 
-            var movieReview = await _context.MovieReviews.FindAsync(id);
+            //find the order detail
+            MovieReview movieReview = await _context.MovieReviews
+                                                   .Include(mr => mr.Movie)
+                                                   .FirstOrDefaultAsync(mr => mr.MovieReviewID == id);
             if (movieReview == null)
             {
-                return NotFound();
+                return View("Error", new String[] { "This movie review was not found. Try creating a review instead!" });
             }
             return View(movieReview);
         }
@@ -90,30 +137,41 @@ namespace Team1_FinalProject.Controllers
         {
             if (id != movieReview.MovieReviewID)
             {
-                return NotFound();
+                return View("Error", new String[] { "There was a problem editing this record. Try again!" });
             }
 
-            if (ModelState.IsValid)
+            //information is not valid, try again
+            if (ModelState.IsValid == false)
             {
-                try
-                {
-                    _context.Update(movieReview);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieReviewExists(movieReview.MovieReviewID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                return View(movieReview);
             }
-            return View(movieReview);
+
+            //create a new order detail
+            MovieReview dbMR;
+            //if code gets this far, update the record
+            try
+            {
+                //find the existing movie review in the database
+                //include movie
+                dbMR = _context.MovieReviews
+                      .Include(rd => rd.Movie)
+                      .FirstOrDefault(rd => rd.MovieReviewID == movieReview.MovieReviewID);
+
+                //update the scalar properties
+                dbMR.MovieRating = movieReview.MovieRating;
+                dbMR.ReviewDescription = movieReview.ReviewDescription;
+
+                //save changes
+                _context.Update(dbMR);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new String[] { "There was a problem editing this record", ex.Message });
+            }
+
+            //if code gets this far, go back to the order details index page
+            return RedirectToAction("Details", "MovieReviews", new { id = dbMR.MovieReviewID });
         }
 
         // GET: MovieReviews/Delete/5
