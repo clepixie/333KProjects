684556ab1700e3f3b40d73141650b3767f4c07a9
Alice Yang <alicejjyang@gmail.com>
2020-11-24 11:26:02 -0600

    Some more annotation stuff

--- Team1_FinalProject/Models/MovieReview.cs
+++ Team1_FinalProject/Models/MovieReview.cs
@@ -9,9 +9,12 @@ namespace Team1_FinalProject.Models
 
 		public Int32 MovieReviewID { get; set; }
 
+		[Required(ErrorMessage = "Please select a rating from 1-5")]
 		[Range(1, 5, ErrorMessage = "Rating must be between 1-5.")]
+		[Display(Name = "Rating")]
 		public Int32 MovieRating { get; set; }
 
+		[Display(Name = "Review")]
 		[StringLength(280, ErrorMessage = "Character limit is 280 characters.")]
 		public string ReviewDescription { get; set; }
 
--- Team1_FinalProject/Models/ViewModels/CheckoutForViewModel.cs
+++ Team1_FinalProject/Models/ViewModels/CheckoutForViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Team1_FinalProject.Models
     public class CheckoutForViewModel
     {
         public int SelectOrderID { get; set; }
+        [Required]
+        [Display(Name = "Available Open Orders")]
         public string SelectOrderName { get; set; }
         public int SelectedOrderID { get; set; }
     }
