785e0fb56f5847deb13b3c7a0d13f0803238fe7f
nadia.baig <70566188+nadbagel@users.noreply.github.com>
2020-11-30 23:31:07 +0300

    migrations

--- Team1_FinalProject/Controllers/ReportsController.cs
+++ Team1_FinalProject/Controllers/ReportsController.cs
@@ -136,7 +136,7 @@ namespace Team1_FinalProject.Controllers
 
             
             svm.SeatsSold = query.Where(t => t.Order.OrderHistory != OrderHistory.Cancelled).Count();
-            List<Ticket> tickets = query.Where(t => t.Order.OrderHistory != OrderHistory.Cancelled).Select(t => t.Order.PostDiscount).ToList();
+            List<Ticket> tickets = query.Where(t => t.Order.OrderHistory != OrderHistory.Cancelled).ToList();
 
             foreach (Ticket ticket in tickets)
             {
