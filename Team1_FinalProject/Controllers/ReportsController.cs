6c3a148a6020d1023fc720d1ae52ce6122271344
aileenlu1163 <aileenlu1163@gmail.com>
2020-11-29 22:25:40 -0600

    reports

--- Team1_FinalProject/Controllers/ReportsController.cs
+++ Team1_FinalProject/Controllers/ReportsController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Team1_FinalProject.DAL;
+using Team1_FinalProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,9 +18,191 @@ namespace Team1_FinalProject.Controllers
     public class ReportsController : Controller
     {
         // GET: /<controller>/
+        private AppDbContext _context;
+        private UserManager<AppUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public ReportsController(AppDbContext dbContext, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            ViewBag.AllCustomers = GetAllCustomers();
+
+            ReportViewModel svm = new ReportViewModel();
+
+            return View(svm);
+        }
+
+        private SelectList GetAllAvailableMovies()
+        {
+            List<Movie> moviesList = _context.Movies.Where(m => m.Showings.Count() != 0).ToList();
+            List<Showing> allShowings = _context.Showings.ToList();
+            /*
+            foreach (Showing showing in allShowings)
+            {
+                // if a showing is older than today
+                if (showing.StartDateTime < DateTime.Now)
+                {
+                    // first see if the showing's movie is still in the list; if not, go to next showing
+                    if (moviesList.Contains(showing.Movie) == false)
+                    {
+                        continue;
+                    }
+
+                    bool nocurrent = true;
+                    // check to see if that showing's movie has any current showings, meaning its start date is >= today
+                    foreach (Showing mshowing in showing.Movie.Showings)
+                    {
+                        if (mshowing.StartDateTime >= DateTime.Now)
+                        {
+                            nocurrent = false;
+                            break;
+                        }
+                    }
+                    if (nocurrent == true)
+                    {
+                        moviesList.Remove(showing.Movie);
+                    }
+                }
+            }
+            */
+            Movie SelectNone = new Movie() { MovieID = 0, Title = "All Movies" };
+            moviesList.Add(SelectNone);
+            SelectList movieSelectList = new SelectList(moviesList.OrderBy(m => m.MovieID), "MovieID", "Title");
+
+            return movieSelectList;
+
+        }
+
+        //select list 
+        private async Task<SelectList> GetAllUsers()
+        {
+            //Get the list of users from the database
+            List<CreateForViewModel> customerList = new List<CreateForViewModel>();
+            List<Int32> customerIDList = new List<Int32>();
+            Int32 count = 0;
+            foreach (AppUser user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, "Customer") == true) //user is in the role
+                {
+                    //add user to list of members
+                    CreateForViewModel newcus = new CreateForViewModel();
+                    newcus.SelectCustomerName = user.Email;
+                    newcus.SelectCustomerID = count;
+                    customerIDList.Add(count);
+                    customerList.Add(newcus);
+                    count += 1;
+                }
+            }
+
+            //convert the list to a SelectList by calling SelectList constructor
+            //MonthID and MonthName are the names of the properties on the Month class
+            //MonthID is the primary key
+            SelectList customerSelectList = new SelectList(customerList, "SelectCustomerID", "SelectCustomerName");
+
+            //return the electList
+            return customerSelectList;
+        }
+
+        [HttpPost]
+        public IActionResult DisplayReport(ReportViewModel svm)
+        {
+            var query = from t in _context.Tickets
+                        select t;
+
+            if (svm.MPAA != null)
+            {
+                query = query.Where(t => t.Showing.Movie.MPAA==svm.MPAA);
+
+            }
+
+            if (svm.CustomerEmail != null)
+            {
+                query = query.Where(t => t.Order.Customer.Email == svm.CustomerEmail);
+            }
+
+            if (svm.PopcornPoints)
+            {
+                query = query.Where(t => t.Order.PopcornPointsUsed == true);
+
+            }
+
+            if (svm.SearchShowingDateStart != null)
+            {
+                DateTime starts = (DateTime)svm.SearchShowingDateStart;
+                if (svm.SearchShowingDateEnd != null)
+                {
+                    DateTime ends = (DateTime)svm.SearchShowingDateEnd;
+                    query = query.Where(t => t.Showing.StartDateTime.Date >= starts && t.Showing.StartDateTime.Date <= ends);
+
+                }
+
+                else
+                {
+                    query = query.Where(t => t.Showing.StartDateTime.Date == starts);
+                }
+            }
+
+            if (svm.SearchShowingDateEnd != null && svm.SearchShowingDateStart == null)
+            {
+                DateTime ends = (DateTime)svm.SearchShowingDateEnd;
+                query = query.Where(t => t.Showing.EndDateTime.Date == ends);
+            }
+
+            if (svm.SearchShowingTimeStart != null)
+            {
+                TimeSpan starts = (TimeSpan)svm.SearchShowingTimeStart;
+                if (svm.SearchShowingDateEnd != null)
+                {
+                    TimeSpan ends = (TimeSpan)svm.SearchShowingTimeEnd;
+                    query = query.Where(t => t.Showing.StartDateTime.TimeOfDay >= starts && t.Showing.EndDateTime.TimeOfDay <= ends);
+
+                }
+
+                else
+                {
+                    query = query.Where(t => t.Showing.StartDateTime.TimeOfDay >= starts);
+                }
+            }
+
+            if (svm.SearchShowingTimeEnd != null && svm.SearchShowingTimeStart == null)
+            {
+                TimeSpan ends = (TimeSpan)svm.SearchShowingTimeEnd;
+                query = query.Where(t => t.Showing.EndDateTime.TimeOfDay <= ends);
+            }
+
+            if (svm.Movie != null && svm.Movie.MovieID != 0)
+            {
+                query = query.Where(t => t.Showing.Movie.MovieID == svm.Movie.MovieID);
+            }
+
+            /*
+            switch (svm.SalaryType)
+            {
+                case SalaryButton.GreaterThan:
+                    query = query.Where(t => t.MinimumSalary >= svm.Salary);
+                    break;
+                case SalaryButton.LessThan:
+                    query = query.Where(t => t.MinimumSalary <= svm.Salary);
+                    break;
+
+            }
+            */
+
+
+            List<JobPosting> SelectedJobPostings = query.Include(jp => jp.Category).ToList();
+
+            //Populate the view bag with a count of all job postings
+            ViewBag.AllJobs = _context.JobPostings.Count();
+            //Populate the view bag with a count of selected job postings
+            ViewBag.SelectedJobs = SelectedJobPostings.Count();
+
+            return View("Index", SelectedJobPostings.OrderByDescending(jp => jp.PostedDate));
+
         }
     }
 }
--- Team1_FinalProject/Models/ViewModels/ReportViewModel.cs
+++ Team1_FinalProject/Models/ViewModels/ReportViewModel.cs
@@ -8,15 +8,40 @@ namespace Team1_FinalProject.Models
 {
     public class ReportViewModel
     {
+        public enum decision
+        {
+            SeatSold, TotalRevenue, Both
+        }
         public ReportViewModel()
         {
         }
-        public Int32 SeatsSold { get; set; }
-        public Decimal TotalRevenue { get; set; }
-        public List<Movie> Movies { get; set; }
-        public List<Showing> Showings { get; set; }
-        public List<Ticket> Tickets { get; set; }
-        public List<Order> Orders { get; set; }
-        
+        public decision Decision { get; set; }
+        public bool PopcornPoints { get; set; }
+        public Movie Movie { get; set; }
+        public MPAA MPAA { get; set; }
+        public string CustomerEmail { get; set; }
+        [Display(Name = "Showing Date:")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime? SearchShowingDateStart { get; set; }
+        [Display(Name = "to:")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime? SearchShowingDateEnd { get; set; }
+        [Display(Name = "Showing Time:")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}")]
+        [DataType(DataType.Time)]
+        public TimeSpan? SearchShowingTimeStart { get; set; }
+        [Display(Name = "to:")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}")]
+        [DataType(DataType.Time)]
+        public TimeSpan? SearchShowingTimeEnd { get; set; }
+
+
+
+        //public List<Showing> Showings { get; set; }
+        //public List<Ticket> Tickets { get; set; }
+        //public List<Order> Orders { get; set; }
+
     }
 }
